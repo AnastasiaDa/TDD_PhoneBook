c54a704738ea565336e747e6e744243d1fb404c9
AnastasiaDa <danilova12333@gmail.com>
2022-10-17 14:37:00 +0300

    Stub and test for addContact

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    static Map<Integer, String> phoneBook = new HashMap<>();
+
+    public static boolean addContact(int phoneNumber, String name) {
+        return false;
+    }
+
+    public static Map<Integer, String> getPhoneBook() {
+        return phoneBook;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBookTest {
+
+    Map<Integer, String> expected = new HashMap<>();
+
+    @Test
+    @DisplayName("Test of adding a new contact")
+    void addContactTest() {
+        PhoneBook.addContact(123_444_555, "Masha");
+        PhoneBook.addContact(123_444_666, "Dasha");
+        PhoneBook.addContact(123_444_777, "Vova");
+        PhoneBook.addContact(123_444_888, "Dima");
+
+        expected.put(123_444_555, "Masha");
+        expected.put(123_444_666, "Dasha");
+        expected.put(123_444_777, "Vova");
+        expected.put(123_444_888, "Dima");
+
+        Map<Integer, String> actual = PhoneBook.getPhoneBook();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
